057c00e2f010917c604c009d715d3bc330ce0a37
Vlad <103497352+4epa@users.noreply.github.com>
2022-09-04 12:33:45 +0300

    Minimo

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+// DOM elements
+const iconMenu = document.querySelector('.burger__logo');
+const menuBody = document.querySelector('.navigate');
+const header = document.querySelector('.header');
+const contentBlock = document.querySelector('.photodiary__container_sidebar');
+const sidebar = document.querySelector('.sitebar__container');
+
+// CONSTANTS
+const scrollTrigger = 60
+
+// CHECK DEVICE
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  IOS: function () {
+    return navigator.userAgent.match(/IOS/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return(
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.IOS() ||
+      isMobile.Windows() ||
+      isMobile.Opera());
+  }
+}
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+} else {
+  document.body.classList.add('_pc');
+}
+
+// BURGER MENU
+iconMenu.addEventListener("click", function(e) {
+  document.body.classList.toggle('_lock');
+  iconMenu.classList.toggle('_active');
+  menuBody.classList.toggle('_active');
+});
+
+// STICKY HEADER
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    header.classList.add('_sticky');
+  } else {
+    header.classList.remove('_sticky');
+  }
+};
+
+// STIKY SITEBAR 
+console.log(contentBlock.scrollHeight)
+console.log(sidebar.clientHeight)
